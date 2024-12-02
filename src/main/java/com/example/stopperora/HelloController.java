f47d30cba168907e30700096287dd3836bd3ec67
Maychii <matyasg760@gmail.com>
2024-12-02 17:12:33 +0100

    fix

--- src/main/java/com/example/stopperora/HelloController.java
+++ src/main/java/com/example/stopperora/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.stopperora;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,15 +8,14 @@ import javafx.scene.control.ListView;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HelloController {
     public Button top;
     public Button bot;
-    public ListView listView;
+    public ListView<String> listView;
 
-    private List<String> items;
 
     @FXML
     private Label time;
@@ -25,54 +23,53 @@ public class HelloController {
     private LocalDateTime now;
 
     private boolean stopped = true;
-    private boolean reset = true;
-
     private Duration timeAlready = Duration.ZERO;
     private Duration duration;
 
+    private Timer timer;
+
     @FXML
     public void initialize() {
-        items = new ArrayList<String>();
         listView.getItems().clear();
+        duration = Duration.ZERO;
     }
 
 
     @FXML
-    protected void onStart(ActionEvent actionEvent) {
+    protected void onStart() {
         stopped = !stopped;
 
-        if(stopped) {
-            timeAlready = duration.plus(timeAlready);
+        if (stopped) {
+            if (timer != null) {
+                timer.cancel();
+            }
+            timeAlready = duration != null ? duration.plus(timeAlready) : timeAlready;
             top.setText("Start");
             bot.setText("Reset");
-        }
-        else{
+        } else {
+            now = LocalDateTime.now();
             top.setText("Stop");
             bot.setText("Részidő");
-        }
 
-        new Thread(() -> {
-            now = LocalDateTime.now();
-            while (!stopped) {
-                LocalDateTime realnow = LocalDateTime.now();
-                duration = Duration.between(now, realnow);
+            timer = new Timer(true);
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    LocalDateTime notNow = LocalDateTime.now();
+                    duration = Duration.between(now, notNow);
 
-                long millis = duration.toMillis() + timeAlready.toMillis();
-                Platform.runLater(() -> time.setText(String.format("%02d:%02d.%03d", millis / (60 * 1000), (millis / 1000) % 60, millis % 1000)));
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+                    long millis = duration.toMillis() + timeAlready.toMillis();
+                    Platform.runLater(() -> time.setText(String.format("%02d:%02d.%03d", millis / (60 * 1000), (millis / 1000) % 60, millis % 1000)));
                 }
-            }
-        }).start();
+            }, 0, 10);
+        }
     }
 
-    public void onStop(ActionEvent actionEvent) {
+    public void onStop() {
         if(stopped){
             timeAlready = Duration.ZERO;
             time.setText("00:00.000");
+            listView.setItems(null);
         }
         else{
             long millis = duration.toMillis() + timeAlready.toMillis();
